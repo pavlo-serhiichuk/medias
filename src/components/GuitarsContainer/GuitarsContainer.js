3c52269f85e04014b7f4edfea77c426f57693ec0
rsundone <pavel.serhiichuk@gmail.com>
2021-11-18 17:26:05 +0200

    Prepared GuitarsContainer.js

--- src/components/GuitarsContainer/GuitarsContainer.js
+++ src/components/GuitarsContainer/GuitarsContainer.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import Products from "../Products/Products.component";
 import {connect} from "react-redux";
+import {getAsyncGuitars} from "../../redux/productsReducer";
+import {addToCart} from "../../redux/cartReducer";
 
 class GuitarsContainer extends React.Component {
 
+    componentDidMount() {
+        this.props.getAsyncGuitars()
+        document.title = 'Guitars| Medias'
+    }
     render() {
         return (
-            <></>
+            <>
+                <Products products={this.props.guitars}
+                          isAuth={this.props.isAuth}
+                          addToCart={this.props.addToCart}/>
+            </>
         );
     }
 }
 
 const mstp = state => ({
-
+    guitars: state.products.books,
+    isAuth: state.auth.isAuth
 })
 
 
-export default connect(mstp, {})(GuitarsContainer);
+export default connect(mstp, {
+    getAsyncGuitars, addToCart
+})(GuitarsContainer);
