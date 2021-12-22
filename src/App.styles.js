d5f760024de5f238d75d35598dd19fb972da8de6
rsundone <pavel.serhiichuk@gmail.com>
2021-12-22 21:00:05 +0200

    [med-8] add-up-down-scroll

--- src/App.styles.js
+++ src/App.styles.js
@@ -16,7 +16,6 @@ export const Wrapper = styled.div`
 
 export const Content = styled.div`
     display: flex;
-    // flex-direction: row;  
     justify-content: center;    
     position: relative;
 `
