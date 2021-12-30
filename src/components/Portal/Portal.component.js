ecc476d544b9e2c7b8a3676168bdff7263cc4b89
spavelo <61013347+spavelo@users.noreply.github.com>
2021-12-30 22:58:18 +0200

    Med 21 create portal window (#12)
    [med-21] Created Portal window.

---
+++ src/components/Portal/Portal.component.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Form, Input, Dialog, SubmitButton, Title, Wrapper, Avatar, Consultant, MessageWrap, Message} from "./Portal.style";
+import {AiOutlineSend as SendButton, SiDialogflow as PortalSign} from "react-icons/all";
+
+const Portal = () => {
+
+    const messageArray = "How can I help you, Sapiens? I'm a big professional in different areas! Know everything about human nature. ".split(' ')
+    const message = messageArray.map(el => <Message>{el}</Message>)
+
+    return (
+        <Wrapper>
+            <Title><PortalSign/>Free support</Title>
+            <Dialog>
+                <Consultant>
+                    <Avatar />
+                    <MessageWrap>{message}</MessageWrap>
+                </Consultant>
+            </Dialog>
+            <Form>
+                <Input />
+                <SubmitButton type="submit">
+                    <SendButton size={35}/>
+                </SubmitButton>
+            </Form>
+        </Wrapper>
+    );
+};
+
+export default Portal;
