c13a308b89d25af987523bc6afcf030c91f4c3f8
spavelo <61013347+spavelo@users.noreply.github.com>
2022-01-05 16:47:17 +0200

    [med-34] fixed logout logic (#26)

--- src/components/Profile/Profile.component.js
+++ src/components/Profile/Profile.component.js
@@ -1,4 +1,5 @@
 import React, {useEffect} from 'react';
+import { useHistory } from "react-router-dom";
 import {Avatar, Content, History, UserInfo, Wrapper} from "./Profile.style";
 import {useDispatch, useSelector} from "react-redux";
 import {closeSidebar} from "../../redux/sidebarReducer";
@@ -6,12 +7,17 @@ import {VscSignOut} from "react-icons/all";
 import Button, {RedButton} from "../common/Buttons/Button.component";
 import {sighOut} from "../../redux/authReducer";
 import {Redirect} from "react-router-dom";
+import {changeCategory} from "../../redux/productsReducer";
 
 const Profile = () => {
-
+    let history = useHistory();
     const dispatch = useDispatch()
     const isAuth = useSelector(state => state.auth.isAuth)
-    const makeSighOut = () => dispatch(sighOut())
+    const makeSighOut = () => {
+        dispatch(sighOut())
+        dispatch(changeCategory('books'))
+        history.push('/books')
+    }
 
     useEffect(() => {
         document.title = 'MyProfile| Medias'
