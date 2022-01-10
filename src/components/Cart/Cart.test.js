2bbb68b1408c43926804a093816f2d2510d834fe
spavelo <61013347+spavelo@users.noreply.github.com>
2022-01-10 22:31:50 +0200

    Med 55 fix header test (#30)
    * [med-53] created jest tests for Cart and  Header components
    
    * [med-55] couldn't fixed it
    
    * [med-55] couldn't fixed it

--- src/components/Cart/Cart.test.js
+++ src/components/Cart/Cart.test.js
@@ -4,14 +4,14 @@ import {render, cleanup} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Cart from "./Cart.component";
 import {createStore} from "redux";
-import {cartReducer} from '../../redux/cartReducer';
 import {Provider} from "react-redux";
+import {rootReducer} from "../../redux/rootReducer";
 
 afterEach(cleanup)
 
 function renderWithRedux(component, {
     initialState,
-    store = createStore(cartReducer, initialState)} = {}) {
+    store = createStore(rootReducer, initialState)} = {}) {
     return {
         ...render(<Provider store={store}>{component}</Provider>)
     }
