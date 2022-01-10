0a1baf510a324c9c6552b02ff4df0a811c882c09
rsundone <pavel.serhiichuk@gmail.com>
2022-01-10 19:20:15 +0200

    [med-53] created jest tests for Cart and  Header components

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -3,14 +3,14 @@ import Provider from "react-redux";
 import {createStore} from "redux";
 import {render, screen, cleanup} from "@testing-library/react";
 import {productsReducer} from "../../redux/productsReducer";
-import {cartReducer} from "../../redux/cartReducer";
 import HeaderContainer from "./HeaderContainer";
 
 afterEach(cleanup)
 
 function renderWithRedux(component, {
     initialState,
-    store = createStore(productsReducer, initialState)} = {}) {
+    store = createStore(productsReducer, initialState)
+} = {}) {
     return {
         ...render(<Provider store={store}>{component}</Provider>)
     }
@@ -19,10 +19,9 @@ function renderWithRedux(component, {
 describe("Header component", () => {
 
     it("renders", () => {
-        renderWithRedux(<HeaderContainer />)
 
-        const link = screen.getByTestId('header')
+        const {getByTestId} = renderWithRedux(<HeaderContainer/>)
 
-        expect(link).toBeInTheDocument()
+        expect(getByTestId('header')).toBeInTheDocument()
     })
 })
