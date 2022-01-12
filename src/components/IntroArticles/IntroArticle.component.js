e670965ace2e346462e0cb914743703205374a90
rsundone <pavel.serhiichuk@gmail.com>
2022-01-12 16:26:59 +0200

    [med-40] Added alternative ways to opening articles

--- src/components/IntroArticles/IntroArticle.component.js
+++ src/components/IntroArticles/IntroArticle.component.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import {ArticleWrapper, Content} from "./IntroArticles.styles";
-import {MiddleTitle} from "../common/Titles/Titles";
+import {ArticleWrapper, Content, MoreInfo, Title} from "./IntroArticles.styles";
 import {Img} from "../common/Imgs/Imgs";
 import {Link} from "react-router-dom";
 
 const IntroArticle = ({article}) => {
+    const articleId = `/article?id=${article.id}`
+
     return (
         <ArticleWrapper>
-            <MiddleTitle>
-                {article.title}
-            </MiddleTitle>
-            <Img bgSize={'500px'} image={article.image} height={'200px'}/>
+            <Title>
+                <Link to={articleId}> {article.title}</Link>
+            </Title>
+            <Link to={articleId}>
+                <Img bgSize={'500px'} image={article.image} height={'200px'}/>
+            </Link>
             <Content>
                 {article.description}
-                <Link to={`/article?id=${article.id}`}>...More info</Link>
+                <Link to={articleId}><MoreInfo>...More info</MoreInfo></Link>
             </Content>
 
         </ArticleWrapper>
